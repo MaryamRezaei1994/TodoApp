24a7bf7d8d872c6f974fcaf33709c99a03111450
Maryam Rezaei <Rezaie.m@entekhabelectronic.com>
2025-12-31 15:42:37 +0330

    Bug fix in create migration

--- TodoApp.Application/obj/Debug/net8.0/TodoApp.Application.AssemblyInfo.cs
+++ TodoApp.Application/obj/Debug/net8.0/TodoApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0782f59117d7d72a4be6901f415d842fc08404e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33148fa6cbbdc97a4624c6d79fdeb7b0a1ee4697")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TodoApp.Infrastructure/Migrations/20251231121028_InitialMig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ToDoApp");
+
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                schema: "ToDoApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Priority = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos",
+                schema: "ToDoApp");
+        }
+    }
+}
